143d03fd52e6adb15b865548a632d2f2ba11e6fe
heejun8250 <gmlwns262@naver.com>
2024-01-05 18:20:50 +0900

    [추가] 타이틀 클릭 시 메인으로 이동, 창넓이 1023px이하일 시 뒤로가기 버튼 추가

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 import SideList from './SideList';
 import SideContents from './SideContents';
+import { Link, useNavigate } from 'react-router-dom';
+import { BiArrowBack } from "react-icons/bi";
 
 
 const Header = () => {
 
   const publicUrl = process.env.PUBLIC_URL;
 
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1)
+  }
+
   return (
     <HeaderArea>
+      <BtnBox>
+        <BackBtn onClick={handleBack}/>
+      </BtnBox>
       {/* <a href='/Portfolio'>🧑‍💻Dev Jun</a> */}
-      <img src={`${publicUrl}/images/webLogo.png`} alt='타이틀 이미지' width={45} height={45}></img>
+      <Link to='/'>
+        <img src={`${publicUrl}/images/webLogo.png`} alt='타이틀 이미지' width={45} height={45}></img>
+      </Link>
       <SideList>
         <SideContents />
       </SideList>
@@ -34,14 +46,45 @@ const HeaderArea = styled.div`
   left: 0px;
   z-index: 10;
 
-    a {
+    /* a {
       color: black;
       font-size: 20px;
       font-weight: 500;
       text-decoration: none;
-    }
+    } */
+
+  img {
+    margin-left: 20px;
+  }
+
+  @media screen and (max-width: 1023px){
+    justify-content: center;
 
     img {
-      margin-left: 20px;
+      margin-left: 0;
     }
+  }
+`
+
+const BtnBox = styled.div`
+  width: 0;
+`
+
+const BackBtn = styled(BiArrowBack)`
+    display: none;
+    position: absolute;
+    width: auto;
+    height: 32px;
+    z-index: 9999999999999;
+    top: 15px;
+    left: 15px;
+    color: black;
+
+  &:hover{
+    color: gray;
+  }
+
+  @media screen and (max-width: 1023px){
+    display: block;
+  }
 `
